e9407ebfb4fe6da93300b458fd9ce0818f1b519b
Dikshit <rahangdaledikshit@gmail.com>
2026-08-25 22:26:54 +0530

    New Changes

--- src/test/java/testCases/AccountRegisteration.java
+++ src/test/java/testCases/AccountRegisteration.java
@@ -26,7 +26,7 @@ public class AccountRegisteration extends DriverSetup {
 		RegistrationPage register = new RegistrationPage(driver);
 		register.enterFirstName("Tst");
 		register.enterLastName("QAs");
-		register.enterEmail("tesshivamogt7@gmail.com");
+		register.enterEmail("tesshivamsdsdsogt7@gmail.com");
 		register.enterTephoneNumber("878762778");
 		register.enterPassword("Te12s@2345");
 		register.enterCnfrmPassword("Te12s@2345");
